b69d5d4b4f9cd9af0ea4c0c7972905000a4eff53
Ankit20321 <ankitmourya20321@acropolis.in>
2024-12-09 18:45:25 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import Homepage from './components/Homepage'
-
-function App() {
-
+import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import Header from './components/Sidebar/Header';
+import { BrowserRouter } from 'react-router-dom'
+import Sidebar from './components/Sidebar/PrimarySidebar';
+const App = () => {
   return (
-    <>
-    <Homepage/>
-      </>
-  )
+      <div>
+    
+        <Header />
+        <Sidebar />
+          <Routes>
+              <Route path='/' element={<Homepage />} />
+            </Routes>
+
+        </div>
+    )
 }
 
 export default App
