84024c032f5ca66d29f9d85e8431973128ff7f13
Ankit20321 <ankitmourya20321@acropolis.in>
2024-12-20 19:56:23 +0530

    20-dec

--- src/components/Pages/Experience/Experience.jsx
+++ src/components/Pages/Experience/Experience.jsx
@@ -1,55 +1,46 @@
-import React from 'react';
-import './experience.css';
-import line101 from './line-10-1.svg';
-import chevron from './double-chevron-right-1.png'
+import React from 'react'
+import '../Education/education.css';
+import line7 from './line-7.svg';
+import line8 from './line-8.svg';
+import line9 from './line-9.svg';
+import vector8 from './vector-8.svg';
+import "../Certification/Certification.css";
 
 const Experience = () => {
   return (
-    <div className='desktop5'>
-      <div className="overlap5">
-      <div className="group-16">
-          <div className="overlap-8">
-            <div className="my-experience">MY EXPERIENCE</div>
-            <div className="group-17">
-              <div className="overlap-group-5">
-                <img
-                  className="double-chevron-right"
-                  alt="Double chevron right"
-                  src={chevron}
-                />
-                <div className="overlap-group-6">
-                  <div className="text-wrapper-18">IIT Indore</div>
-                  <img className="line-9" alt="Line" src={line101} />
-                </div>
-                <div className="text-wrapper-19">Research Analyst</div>
-                <div className="text-wrapper-20">Aug 2022-Oct 2022</div>
-                <p className="text-wrapper-21">
-                  I am designing a simple website having similar homepage that of Netflix using using HTML, CSS and
-                  JavaScript.
-                </p>
-              </div>
-            </div>
-            <div className="group-18">
-              <div className="overlap-group-5">
-                <img
-                  className="double-chevron-right"
-                  alt="Double chevron right"
-                  src={chevron}
-                />
-                <div className="overlap-group-6">
-                  <div className="text-wrapper-18">Oasis Infobyte</div>
-                  <img className="line-9" alt="Line" src={line101} />
-                </div>
-                <div className="text-wrapper-19">Website Developer</div>
-                <div className="text-wrapper-22">January-Feburary</div>
-                <p className="text-wrapper-21">I had designed a website of</p>
-              </div>
-            </div>
+    <div className='desktop4'>
+      <div className='overlap3'>
+      <div className="my-education">MY Experience</div>
+      <div className="group-15">
+        <div className="certification-scroller">
+          <div className="overlap-7">
+            <div className="rectangle-6" />
+            <p className="text-wrapper-12">
+              Bachelor in Technology in Computer Science and Engineering - Data Science: 7.97 (CGPA)
+            </p>
+            <p className="text-wrapper-13">Higher Secondary (12th State Board):- 72.8 %</p>
+            <p className="text-wrapper-14">Senior Secondary (10th State Board):- 85.4%</p>
+            <p className="acropolis-institute">ACROPOLIS INSTITUTE OF TECHNOLOGY AND RESEARCH, INDORE</p>
+            <div className="vidyasagar-school">Mount Carmel H.S. School, INDORE</div>
+            <div className="pratibhasthali">Mount Carmel H.S. School, Indore</div>
+            <div className="text-wrapper-15">2020-2024</div>
+            <div className="text-wrapper-16">2019-2020</div>
+            <div className="text-wrapper-17">2017-2018</div>
+            <div className="ellipse" />
+            <div className="ellipse-2" />
+            <div className="ellipse-3" />
+            <img className="line-6" alt="Line" src={line7} />
+            <img className="line-7" alt="Line" src={line8} />
+            <img className="line-8" alt="Line" src={line9} />
+            <img className="vector-7" alt="Vector" src={vector8} />
+            <img className="vector-8" alt="Vector" src={vector8} />
+            <img className="vector-9" alt="Vector" src={vector8} />
+          </div>
           </div>
         </div>
-      </div>
+        </div>
     </div>
   )
 }
 
-export default Experience
+export default Experience;
